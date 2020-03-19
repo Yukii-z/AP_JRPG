3069052cf9b8d4479a5e0b36aa00dc78d318a3c6
Yukii <yukiizzzx@gmail.com>
2020-03-19 16:51:01 -0400

    Game

---
+++ Assets/Scripts/Model.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Model : MonoBehaviour
+{
+    public static Model Instance;
+    public GameObject Enemy;
+    public GameObject Friends;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum CharacterType
+{
+    Enemy,
+    Friend,
+}
+public class CharacterObject
+{
+    public CharacterObject(string name, CharacterType type)
+    {
+        var obj = new GameObject();
+        obj.transform.parent = type == CharacterType.Friend
+            ? Model.Instance.Friends.transform
+            : Model.Instance.Enemy.transform;
+        obj.name = name;
+    }
+}
